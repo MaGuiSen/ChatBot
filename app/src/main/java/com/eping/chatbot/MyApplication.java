4b7825494808f3dbe350969d91ad2436be8115cb
匿名者475 <1059876295@qq.com>
2017-08-22 19:56:56 +0800

    修改细节

--- app/src/main/java/com/eping/chatbot/MyApplication.java
+++ app/src/main/java/com/eping/chatbot/MyApplication.java
@@ -6,9 +6,9 @@ import com.qeelyn.chatbot.api.BotClient;
 
 public class MyApplication extends Application {
 	private String clientSecret = "123456";//密钥id
-	private String clientId = "eping";//客户端类型
+	private String clientId = "wdxx123456!";//客户端类型
     private String grantType = "client_credentials";//验证类型
-    private String baseUrl = "http://117.29.166.222:8099";//chatbot基础地址
+    private String baseUrl = "http://test.qeelyn.hk";//chatbot基础地址
 	private static MyApplication mMyApplication;
 
 	public static MyApplication getInstance(){
